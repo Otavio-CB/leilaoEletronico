19354c6bd59d2bc244f6ee7426e3c91274832d96
ocalderan <otaviocalderanadm@gmail.com>
2024-09-27 18:04:18 -0300

    📦️ (chore): Add Lance class

---
+++ src/main/java/br/gov/sp/fatec/lp2/Lance.java
@@ -0,0 +1,21 @@
+package br.gov.sp.fatec.lp2;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Lance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double valor;
+
+    @ManyToOne
+    private Cliente cliente;
+
+    @ManyToOne
+    private Leilao leilao;
+}
