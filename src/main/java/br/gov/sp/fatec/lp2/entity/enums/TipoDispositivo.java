7cd9dcd1cd4864d5d4f68f49c21cd2e337713ed0
ocalderan <otaviocalderanadm@gmail.com>
2024-10-09 00:08:28 -0300

    ✨ (feat): Add enums of Veiculo and Dispositivo

---
+++ src/main/java/br/gov/sp/fatec/lp2/entity/enums/TipoDispositivo.java
@@ -0,0 +1,9 @@
+package br.gov.sp.fatec.lp2.entity.enums;
+
+public enum TipoDispositivo {
+    NOTEBOOK,
+    MONITOR,
+    HUB,
+    SWITCH,
+    ROTEADOR
+}
---
+++ src/main/java/br/gov/sp/fatec/lp2/entity/enums/TipoVeiculo.java
@@ -0,0 +1,8 @@
+package br.gov.sp.fatec.lp2.entity.enums;
+
+public enum TipoVeiculo {
+    CARRO,
+    MOTOCICLETA,
+    CAMINHAO,
+    UTILITARIO
+}
