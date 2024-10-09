8ec4ce642ec10f6721468f5d9a36f74c44c94fd7
ocalderan <otaviocalderanadm@gmail.com>
2024-10-09 09:50:42 -0300

    🚧 (feat): Implementing H2

--- src/main/java/br/gov/sp/fatec/lp2/entity/Cliente.java
+++ src/main/java/br/gov/sp/fatec/lp2/entity/Cliente.java
@@ -1,27 +1,27 @@
-    package br.gov.sp.fatec.lp2.entity;
+package br.gov.sp.fatec.lp2.entity;
 
-    import jakarta.persistence.*;
-    import lombok.Data;
+import jakarta.persistence.*;
+import lombok.Data;
 
-    import java.util.List;
+import java.util.List;
 
-    @Entity
-    @Data
-    public class Cliente {
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
+@Entity
+@Data
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-        private String nome;
-        private String email;
-        private String senha;
-
-
-        @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
-        private List<Lance> lances;
+    private String nome;
+    private String email;
+    private String senha;
 
 
-        @ManyToMany
-        @JoinTable(name = "cliente_favoritos", joinColumns = @JoinColumn(name = "cliente_id"), inverseJoinColumns = @JoinColumn(name = "leilao_id"))
-        private List<Leilao> produtosFavoritos;
-    }
+    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
+    private List<Lance> lances;
+
+
+    @ManyToMany
+    @JoinTable(name = "cliente_favoritos", joinColumns = @JoinColumn(name = "cliente_id"), inverseJoinColumns = @JoinColumn(name = "leilao_id"))
+    private List<Leilao> produtosFavoritos;
+}
