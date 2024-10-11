21ff9f8dcb3d77f163dc9ad64599c51e425f1888
ocalderan <otaviocalderanadm@gmail.com>
2024-10-11 16:51:00 -0300

    ⚰️ (remove): Remove useless method

--- src/main/java/br/gov/sp/fatec/lp2/repository/ClienteRepository.java
+++ src/main/java/br/gov/sp/fatec/lp2/repository/ClienteRepository.java
@@ -4,9 +4,7 @@ import br.gov.sp.fatec.lp2.entity.Cliente;
 import io.micronaut.data.annotation.Repository;
 import io.micronaut.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-    Optional<Cliente> findByEmail(String email);
 }
