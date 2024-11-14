6c4ef85c91483855397a78968eba8b709b532253
ocalderan <otaviocalderanadm@gmail.com>
2024-11-14 18:56:14 -0300

    ♻️ (refactor): Remove unused methods

--- src/main/java/br/gov/sp/fatec/lp2/repository/LanceRepository.java
+++ src/main/java/br/gov/sp/fatec/lp2/repository/LanceRepository.java
@@ -1,7 +1,6 @@
 package br.gov.sp.fatec.lp2.repository;
 
 import br.gov.sp.fatec.lp2.entity.Lance;
-import br.gov.sp.fatec.lp2.entity.Leilao;
 import io.micronaut.data.annotation.Repository;
 import io.micronaut.data.jpa.repository.JpaRepository;
 
@@ -10,5 +9,6 @@ import java.util.List;
 @Repository
 public interface LanceRepository extends JpaRepository<Lance, Long> {
     List<Lance> findByDispositivoId(Long dispositivoId);
+
     List<Lance> findByVeiculoId(Long veiculoId);
 }
